3eff36ffd0b2759b77dda450dd8920efd68a646c
FrygiDrygi <jgemels@gmail.com>
2017-08-13 13:50:22 +0200

    przeniesieni reakcji na zdarzenia klawiatury i myszki do game-functions.py

--- alien.invasion.py
+++ alien.invasion.py
@@ -1,10 +1,9 @@
-import sys
-
 import pygame
 
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -20,11 +19,7 @@ def run_game():
 
     # Rozpoczęcie pętli głównej gry
     while True:
-
-        # Oczekiwnaie na naciśnięcie klaiwsza lub przycisk myszy
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # Odświeżenie ekranu w trakcie każdej iteracji pętli
         screen.fill(ai_settings.bg_color)
